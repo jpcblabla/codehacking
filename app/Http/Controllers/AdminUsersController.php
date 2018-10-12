c7a7a6594a805195449bc63a2395e6d23a219730
jpcblabla <joaopedrocamacho@gmail.com>
2018-10-12 12:42:02 +0100

    users with uploading features working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\User;
 use App\Role;
 use Illuminate\Http\Request;
@@ -51,7 +52,22 @@ class AdminUsersController extends Controller
     {
         //
 
-            User::create($request->all());
+            $input = $request->all();
+
+            if($file = $request->file('photo_id')){
+
+                $name = time(). $file->getClientOriginalName();
+
+                $file->move('images', $name);
+
+                $photo = Photo::create(['file'=>$name]);
+
+                $input['photo_id'] = $photo->id;
+
+            }
+
+            $input['password'] = bcrypt($request->password);
+            User::create($input);
 
 
 
