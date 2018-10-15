ae0ae92c915c8b3126d1075e423722a0a5ce4028
jpcblabla <joaopedrocamacho@gmail.com>
2018-10-15 13:38:52 +0100

    able to create posts with dynamic categorieSs

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Http\Requests\PostsCreateRequest;
+use App\Photo;
+use App\Post;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
 
 class AdminPostsController extends Controller
 {
@@ -16,6 +21,10 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
+
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -26,6 +35,10 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
+
+        $cat = Category::lists('name', 'id')->all();
+
+        return view('admin.posts.create', compact('cat'));
     }
 
     /**
@@ -34,9 +47,33 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+
+        $input = $request->all();
+
+        $user = Auth::user();
+
+        if($file = $request->file('photo_id')) {
+
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->posts()->create($input);
+
+       return redirect('/admin/posts');
+
+
+
+
     }
 
     /**
--- app/User.php
+++ app/User.php
@@ -54,4 +54,12 @@ class User extends Authenticatable
         return false;
 
     }
+
+    public function posts(){
+
+
+        return $this->hasMany('App\Post');
+
+    }
+
 }
